1493b48f945900b6e2526686088098d7d5bb810d
luana2322 <thailuanpy1234@gmai.com>
2025-11-06 11:36:26 +0700

    9.5

--- flutter_crud/lib/utils/constants.dart
+++ flutter_crud/lib/utils/constants.dart
@@ -4,15 +4,15 @@ import 'package:flutter/foundation.dart';
 class AppConfig {
   static String getBaseUrl() {
     if (kIsWeb) return 'http://localhost:8080/api'; // Flutter Web
-    if (Platform.isAndroid) return 'http://172.16.0.201:8080/api'; // Android Emulator
-    if (Platform.isIOS) return 'http://172.16.0.201:8080/api'; // iOS simulator
+    if (Platform.isAndroid) return 'http://172.16.0.106:8080/api'; // Android Emulator
+    if (Platform.isIOS) return 'http://172.16.0.106:8080/api'; // iOS simulator
     return 'http://localhost:8080/api'; // Windows/Mac/Linux
   }
 
   static String getMinioUrl() {
     if (kIsWeb) return 'http://localhost:9000';
-    if (Platform.isAndroid) return 'http://172.16.0.201:9000';
-    if (Platform.isIOS) return 'http://127.0.0.1:9000';
+    if (Platform.isAndroid) return 'http://172.16.0.106:9000';
+    if (Platform.isIOS) return 'http://172.16.0.106:9000';
     return 'http://localhost:9000';
   }
 }
